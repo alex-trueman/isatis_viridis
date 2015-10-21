38f9e21c1596c01074217aaa7ae97647ffd669cd
Alex Trueman <amt4158@gmail.com>
2015-10-21 08:32:59 -0600

    Update isatis_palette_generator.R
    Added path variable and additional comments.

--- R/isatis_palette_generator.R
+++ R/isatis_palette_generator.R
@@ -10,78 +10,60 @@ make_isatis_palette <- function(rgb_table, title, path = "./"){
   return(paste0(path, title, ".pal has been created"))
 }
 
+path <- "D:/Dropbox/_system/isatis/legends/"
+
 # Make viridis pallet files of various levels.
+## Generate a table of RGB values. Use viridis package to generate
+## table of hex colours. Transform this with RColorBrewer's col2rgb.
+## Finally translate to format used by Isatis.
 rgb_table <- t(col2rgb(viridis(256, 1, option = "viridis")))
-make_isatis_palette(rgb_table, "viridis_256", "D:/Dropbox/_system/isatis/legends/")
+## Write the RGB colour table and title to Isatis palette format.
+make_isatis_palette(rgb_table, "viridis_256", path)
 
 rgb_table <- t(col2rgb(viridis(128, 1, option = "viridis")))
-make_isatis_palette(rgb_table, "viridis_128", "D:/Dropbox/_system/isatis/legends/")
+make_isatis_palette(rgb_table, "viridis_128", path)
 
 rgb_table <- t(col2rgb(viridis(64, 1, option = "viridis")))
-make_isatis_palette(rgb_table, "viridis_064", "D:/Dropbox/_system/isatis/legends/")
+make_isatis_palette(rgb_table, "viridis_064", path)
 
 rgb_table <- t(col2rgb(viridis(32, 1, option = "viridis")))
-make_isatis_palette(rgb_table, "viridis_032", "D:/Dropbox/_system/isatis/legends/")
-
-rgb_table <- t(col2rgb(viridis(16, 1, option = "viridis")))
-make_isatis_palette(rgb_table, "viridis_016", "D:/Dropbox/_system/isatis/legends/")
-
-rgb_table <- t(col2rgb(viridis(11, 1, option = "viridis")))
-make_isatis_palette(rgb_table, "viridis_011", "D:/Dropbox/_system/isatis/legends/")
+make_isatis_palette(rgb_table, "viridis_032", path)
 
 # Make magma pallet files of various levels.
 rgb_table <- t(col2rgb(viridis(256, 1, option = "magma")))
-make_isatis_palette(rgb_table, "magma_256", "D:/Dropbox/_system/isatis/legends/")
+make_isatis_palette(rgb_table, "magma_256", path)
 
 rgb_table <- t(col2rgb(viridis(128, 1, option = "magma")))
-make_isatis_palette(rgb_table, "magma_128", "D:/Dropbox/_system/isatis/legends/")
+make_isatis_palette(rgb_table, "magma_128", path)
 
 rgb_table <- t(col2rgb(viridis(64, 1, option = "magma")))
-make_isatis_palette(rgb_table, "magma_064", "D:/Dropbox/_system/isatis/legends/")
+make_isatis_palette(rgb_table, "magma_064", path)
 
 rgb_table <- t(col2rgb(viridis(32, 1, option = "magma")))
-make_isatis_palette(rgb_table, "magma_032", "D:/Dropbox/_system/isatis/legends/")
-
-rgb_table <- t(col2rgb(viridis(16, 1, option = "magma")))
-make_isatis_palette(rgb_table, "magma_016", "D:/Dropbox/_system/isatis/legends/")
-
-rgb_table <- t(col2rgb(viridis(11, 1, option = "magma")))
-make_isatis_palette(rgb_table, "magma_011", "D:/Dropbox/_system/isatis/legends/")
+make_isatis_palette(rgb_table, "magma_032", path)
 
 # Make inferno pallet files of various levels.
 rgb_table <- t(col2rgb(viridis(256, 1, option = "inferno")))
-make_isatis_palette(rgb_table, "inferno_256", "D:/Dropbox/_system/isatis/legends/")
+make_isatis_palette(rgb_table, "inferno_256", path)
 
 rgb_table <- t(col2rgb(viridis(128, 1, option = "inferno")))
-make_isatis_palette(rgb_table, "inferno_128", "D:/Dropbox/_system/isatis/legends/")
+make_isatis_palette(rgb_table, "inferno_128", path)
 
 rgb_table <- t(col2rgb(viridis(64, 1, option = "inferno")))
-make_isatis_palette(rgb_table, "inferno_064", "D:/Dropbox/_system/isatis/legends/")
+make_isatis_palette(rgb_table, "inferno_064", path)
 
 rgb_table <- t(col2rgb(viridis(32, 1, option = "inferno")))
-make_isatis_palette(rgb_table, "inferno_032", "D:/Dropbox/_system/isatis/legends/")
-
-rgb_table <- t(col2rgb(viridis(16, 1, option = "inferno")))
-make_isatis_palette(rgb_table, "inferno_016", "D:/Dropbox/_system/isatis/legends/")
-
-rgb_table <- t(col2rgb(viridis(11, 1, option = "inferno")))
-make_isatis_palette(rgb_table, "inferno_011", "D:/Dropbox/_system/isatis/legends/")
+make_isatis_palette(rgb_table, "inferno_032", path)
 
 # Make plasma pallet files of various levels.
 rgb_table <- t(col2rgb(viridis(256, 1, option = "plasma")))
-make_isatis_palette(rgb_table, "plasma_256", "D:/Dropbox/_system/isatis/legends/")
+make_isatis_palette(rgb_table, "plasma_256", path)
 
 rgb_table <- t(col2rgb(viridis(128, 1, option = "plasma")))
-make_isatis_palette(rgb_table, "plasma_128", "D:/Dropbox/_system/isatis/legends/")
+make_isatis_palette(rgb_table, "plasma_128", path)
 
 rgb_table <- t(col2rgb(viridis(64, 1, option = "plasma")))
-make_isatis_palette(rgb_table, "plasma_064", "D:/Dropbox/_system/isatis/legends/")
+make_isatis_palette(rgb_table, "plasma_064", path)
 
 rgb_table <- t(col2rgb(viridis(32, 1, option = "plasma")))
-make_isatis_palette(rgb_table, "plasma_032", "D:/Dropbox/_system/isatis/legends/")
-
-rgb_table <- t(col2rgb(viridis(16, 1, option = "plasma")))
-make_isatis_palette(rgb_table, "plasma_016", "D:/Dropbox/_system/isatis/legends/")
-
-rgb_table <- t(col2rgb(viridis(11, 1, option = "plasma")))
-make_isatis_palette(rgb_table, "plasma_011", "D:/Dropbox/_system/isatis/legends/")
+make_isatis_palette(rgb_table, "plasma_032", path)
